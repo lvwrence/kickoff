6128ebbe0f9a6211bac75dcfdf5fe0611911931b
Lawrence Wu <lawrence@lawrencewu.me>
2016-01-26 23:45:59 -0800

    Update dependencies and add class props support

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,29 @@
 module.exports = {
-    "rules": {
-        "indent": [
-            2,
-            4
-        ],
-        "quotes": [
-            2,
-            "single"
-        ],
-        "linebreak-style": [
-            2,
-            "unix"
-        ],
-    },
-    "env": {
-        "es6": true,
-        "browser": true
-    },
-    "extends": "airbnb",
-    "ecmaFeatures": {
-        "jsx": true,
-        "experimentalObjectRestSpread": true
-    },
-    "plugins": [
-        "react"
-    ]
+  'parser': 'babel-eslint',
+  'rules': {
+      'indent': [
+          2,
+          4
+      ],
+      'quotes': [
+          2,
+          'single'
+      ],
+      'linebreak-style': [
+          2,
+          'unix'
+      ],
+  },
+  'env': {
+      'es6': true,
+      'browser': true
+  },
+  'extends': 'airbnb',
+  'ecmaFeatures': {
+      'jsx': true,
+      'experimentalObjectRestSpread': true
+  },
+  'plugins': [
+      'react'
+  ]
 };
