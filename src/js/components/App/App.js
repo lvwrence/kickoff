e84bb1e96991b5edd49b0b58effffe1cf279b708
Lawrence Wu <lawrence@lawrencewu.me>
2016-01-26 23:02:34 -0800

    Lorem ipsum in Header

--- src/js/components/App/App.js
+++ src/js/components/App/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Header from 'components/Header/Header';
 
 export default class App extends Component {
   render() {
       return (
-      <div>
-        <header>heyyyy</header>
-      </div>
+        <div>
+          <Header />
+        </div>
     );
   }
 }
